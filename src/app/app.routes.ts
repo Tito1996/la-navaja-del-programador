df849cbaff2669252797aeff3b2d3299580810c9
Vicente <vicente.ict3@gmail.com>
2026-01-15 19:32:58 +0100

    toolls

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    pathMatch: 'full',
+    loadComponent: () =>
+      import('./features/home/home').then(m => m.Home),
+  },
+  {
+    path: 'tools',
+    loadChildren: () =>
+      import('./features/tools/tools.routes').then(m => m.TOOLS_ROUTES),
+  },
+  {
+    path: 'favorites',
+    loadComponent: () =>
+      import('./features/favorites/favorites').then(m => m.Favorites),
+  },
+  {
+    path: 'history',
+    loadComponent: () =>
+      import('./features/history/history').then(m => m.History),
+  },
+  {
+    path: 'settings',
+    loadComponent: () =>
+      import('./features/settings/settings').then(m => m.Settings),
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./features/not-found/not-found').then(m => m.NotFound),
+  },
+];
